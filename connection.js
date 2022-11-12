7633343f048f857dde04d38f1efb52aad894ad6f
Chandan Bauri <chandanbauri441@gmail.com>
2022-11-12 22:31:55 +0530

    updated enter page

--- connection.js
+++ connection.js
@@ -1,18 +1,18 @@
 const {Client} = require('pg');
 
-const client = new Client({
-    host: "localhost",
-    user: "rb",
-    port: 5432,
-    password: 'richbudget', //"21HU!!mk#",
-    database: "richbudget"
-})
-
 // const client = new Client({
 //     host: "localhost",
-//     user: "osdm",
+//     user: "rb",
 //     port: 5432,
-//     password: 'osdm007', //"21HU!!mk#",
-//     database: "postgres"
+//     password: 'richbudget', //"21HU!!mk#",
+//     database: "richbudget"
 // })
+
+const client = new Client({
+    host: "localhost",
+    user: "osdm",
+    port: 5432,
+    password: 'kvosdm007$', //"21HU!!mk#",
+    database: "postgres"
+})
 module.exports = client
